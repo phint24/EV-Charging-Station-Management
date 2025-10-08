a6832a18e4d5484b592f26bad5d67e9ffd8fc032
phint24 <nguyentanphi24022005@gmail.com>
2025-10-08 15:05:01 +0700

    update base project

---
+++ Backend/evcharging.backend/src/test/java/java/code/evcharging/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package java.code.evcharging.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
